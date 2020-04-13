10d494885c06b1606acc2a46eda5da9065e95448
luckyjialing <jialing.wu.gr@dartmouth.edu>
2020-04-13 18:37:41 -0400

    jialingiswonderful

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import $ from 'jquery';
+import './style.scss';
+
 console.log('starting up!');
+
+// const $ = require('jquery');
+$('#main').html('Here we go!');
+
+let num = 0;
+
+setInterval(() => {
+  num += 1;
+  $('#main').html(`You've been on this page for ${num} seconds`);
+}, 1000);
