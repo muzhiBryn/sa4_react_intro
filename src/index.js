8a0b274408a7d832f4cd8f76e005acea75acfa1f
luckyjialing <jialing.wu.gr@dartmouth.edu>
2020-04-16 00:49:31 -0400

    StarterPack

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
 import './style.scss';
 
 const App = () => <div className="test">All the REACT are belong to us!</div>;
 
 ReactDOM.render(<App />, document.getElementById('main'));
-
-console.log('starting up!');
-
-// const $ = require('jquery');
-$('#main').html('Here we go!');
-
-let num = 0;
-
-setInterval(() => {
-  num += 1;
-  $('#main').html(`You've been on this page for ${num} seconds`);
-}, 1000);
