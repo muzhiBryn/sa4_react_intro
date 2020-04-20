b7c12ffd786313be9085feb6c5dff403e3a21ee4
luckyjialing <jialing.wu.gr@dartmouth.edu>
2020-04-19 21:53:03 -0400

    add yarn deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
+import debounce from 'lodash.debounce';
+import SearchBar from './components/search_bar';
+import youtubeSearch from './youtube-api';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+    this.search = debounce(this.search, 300);
+    this.search('pixar');
+  }
+
+  search = (text) => {
+    youtubeSearch(text).then((videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchChange={this.search} />
+        <div id="video-section">
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })} videos={this.state.videos} />
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('main'));
